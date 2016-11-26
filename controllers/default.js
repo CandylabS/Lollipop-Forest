a04d1303f6586a591bc834866fb8a16a6084aec2
Alice Chen <alice911003@gmail.com>
2016-11-26 15:01:17 -0500

    test routing and map

--- controllers/default.js
+++ controllers/default.js
@@ -1,7 +1,15 @@
 exports.install = function(){
-	F.route('/');
+	F.route('/', viewTheme);
 	F.route('/paper/', viewPaper);
 	// F.redirect('/', 'paper/examples/Paperjs.org/SatieLikedToDraw.html');
+	F.map('/custom/', '~/public/js/')
+}
+
+function viewTheme(){
+	var self = this;
+	self.theme(self.query.theme || 'light');
+	self.title('Lollipop Forest');
+	this.view('index');
 }
 
 function viewPaper(){
